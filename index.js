459628fe708eac12cb9dff9e3e2f18a953ebf548
cooljasonmelton <cooljasonmelton>
2020-03-31 00:07:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+element = document.querySelector('p#text')
+
+document.addEventListener("DOMContentLoaded", function() {
+    element.innerHTML = "This is really cool!";
+
+});
